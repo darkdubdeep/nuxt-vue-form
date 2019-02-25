5b83e89100b5039b0c6a8119cba43a6a8c45d6ee
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2019-02-25 10:27:37 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div>
+      <app-form/>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppForm from "~/components/AppForm.vue";
+
+export default {
+  components: {
+    AppForm
+  }
+};
+</script>
+
+<style>
+</style>
+
